dad817ef9a47ddfe643c589c23d2e2f9069ad252
dharshinithangavel080 <dharshinithangavel080@gmail.com>
2026-08-20 07:28:02 +0530

    NeonNumber

---
+++ NeonNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class NeonNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int square = n * n;
+        int sum = 0;
+
+        while (square > 0) {
+            sum = sum + square % 10;
+            square = square / 10;
+        }
+
+        if (sum == n)
+            System.out.println("Neon Number");
+        else
+            System.out.println("Not Neon Number");
+    }
+}

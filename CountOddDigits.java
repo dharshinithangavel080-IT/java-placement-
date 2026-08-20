73b9788df57d865bcbb0051a11d97f5749f710c8
dharshinithangavel080 <dharshinithangavel080@gmail.com>
2026-08-20 10:57:46 +0530

    CountOddDigits

---
+++ CountOddDigits.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CountOddDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int count = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+
+            if (digit % 2 != 0)
+                count++;
+
+            n = n / 10;
+        }
+
+        System.out.println("Odd digits count = " + count);
+    }
+}

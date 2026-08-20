7451c4ea385ecae61bc7065dde5967f815a86eeb
dharshinithangavel080 <dharshinithangavel080@gmail.com>
2026-08-20 11:04:31 +0530

    LargestDigit

---
+++ LargestDigit.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LargestDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int max = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+
+            if (digit > max)
+                max = digit;
+
+            n = n / 10;
+        }
+
+        System.out.println("Largest digit = " + max);
+    }
+}
